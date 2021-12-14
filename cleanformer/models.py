8c1faab109ffd1ee7b00eb4f7474498c29a907a3
jl749 <wjdan7051@gmail.com>
2021-12-14 20:56:57 +0900

    implemented training step

--- cleanformer/models.py
+++ cleanformer/models.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple
+import torch
 from pytorch_lightning import LightningModule
 
 
@@ -8,5 +10,47 @@ class Transformer(LightningModule):
                  dropout: float, lr: float):  # noqa
         super().__init__()
         self.save_hyperparameters()
+        
         # TODO: implement transformer
-        raise NotImplementedError
+        self.token_embeddings = torch.nn.Embedding(num_embeddings=vocab_size, embedding_dim=hidden_size)  # vocab_size -> hidden_size
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, src_ids: torch.LongTensor, tgt_ids: torch.Tensor,
+                    src_key_padding_mask: torch.Tensor, tgt_key_padding_mask: torch.Tensor) -> torch.Tensor:
+        src = self.token_embeddings(src_ids)  # linear layer (N, L) --> (N, L, H)
+        tgt = self.token_embeddings(tgt_ids)  # (N, L) --> (N, L, H)
+
+        # POSITIONAL ENCODING
+        # TODO: later
+
+        memory = self.encoder.forward(src)  # (N, L, H) --> (N, L, H)
+        hidden = self.decoder.forward(tgt, memory)  # (N, L, H) --> (N, L, H)
+        return hidden
+
+    # override
+    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], **kwargs) -> Dict:
+        X, Y = batch  # (N, 2, 2, L) (N, L)
+        # [ N = batch size, 2(src(KO) / target(ENG)), 2(ids / mask(padding)), L(sequence max length) ]
+        
+        # encoder inputs
+        src_ids, src_key_padding_mask = X[:, 0, 0], X[:, 0, 1]
+        # decoder inputs
+        tgt_ids, tgt_key_padding_mask = X[:, 1, 0], X[:, 1, 1]
+
+        hidden = self.forward(src_ids, tgt_ids, src_key_padding_mask, tgt_key_padding_mask)
+        classifier = self.token_embeddings.weight  # (V, H)  V = BoW classes
+        logits = torch.einsum("nlh,vh->nvl", hidden, classifier)
+        loss = torch.nn.functional.cross_entropy(logits, Y)  # CEE requires (N, classes, L), (N, L)
+        loss = loss.sum()  # (N,) -> (,)
+        
+        return {
+            "loss": loss,
+        }
+
+
+class Encoder(torch.nn.Module):
+    raise NotImplementedError
+
+class Decoder(torch.nn.Module):
+    raise NotImplementedError
